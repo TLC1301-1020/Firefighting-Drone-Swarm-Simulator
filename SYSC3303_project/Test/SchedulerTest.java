e8d7e0cbdf08657c98cd4cd898e7a9c0b52ed17b
andrew-tawfik <and.tawfik07@gmail.com>
2025-02-20 16:53:26 -0500

    Pull from Tina's branch

--- SYSC3303_project/Test/SchedulerTest.java
+++ SYSC3303_project/Test/SchedulerTest.java
@@ -1,5 +1,5 @@
-import static org.junit.Assert.*;
-import org.junit.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.*;
 
 /**
  * {@code SchedulerTest} contains junit unit tests for the {@code Scheduler} class correctly
@@ -7,33 +7,69 @@ import org.junit.*;
  */
 public class SchedulerTest {
 
+
     /**
-     * tests the {@code addRequest} and {@code takeRequest} methods
-     * ensures that a fire request is correctly added to and retrieved from the scheduler
+     * tests the {@code addRequest} method
+     * ensures that a fire request is correctly added to the scheduler
      */
     @Test
-    public void testRequest() {
-        System.out.println("Testing addRequest and takeRequest function:");
+    public void Test_addRequest() {
+        System.out.println("Test: adding Request to Scheduler");
         Scheduler scheduler = new Scheduler();
         FireRequest request = new FireRequest("06:20:19", 3, "FIRE_DETECTED", "Low");
         scheduler.addRequest(request);
-        assertEquals(request, scheduler.takeRequest());
+
+        Assertions.assertEquals(request, scheduler.takeRequest());
+
     }
 
     /**
-     * tests the {@code addResponse} and {@code takeResponse} methods
-     * ensures that a response is correctly added to and retrieved from the scheduler
+     * tests the {@code takeRequest} method
+     * ensures that the request is correctly taken by the scheduler
      */
     @Test
-    public void testResponse() {
-        System.out.println("Testing addResponse and takeResponse function:");
+    public void Test_takeRequest() {
+        System.out.println("Test: taking Request from Scheduler");
         Scheduler scheduler = new Scheduler();
         FireRequest request = new FireRequest("06:20:19", 3, "FIRE_DETECTED", "Low");
         scheduler.addRequest(request);
-        System.out.println("added request" + request);
-        Response response = new Response(request, "completed");
 
+        Assertions.assertEquals(request, scheduler.takeRequest());
+
+    }
+
+    /**
+     * tests the {@code addResponse} method
+     * ensures the response to the current task is sent to the scheduler
+     */
+    @Test
+    public void Test_addResponse(){
+        System.out.println("Test: adding response to scheduler");
+        Scheduler scheduler = new Scheduler();
+        FireRequest request = new FireRequest("06:20:19", 3, "FIRE_DETECTED", "Low");
+        scheduler.addRequest(request);
+        Response response = new Response(request,"completed");
         scheduler.addResponse(response);
-        assertEquals(response, scheduler.takeResponse());
+
+        Assertions.assertEquals(scheduler.getCurrentResponse(),response);
     }
+
+    /**
+     * tests the {@code takeResponse} method
+     * ensures the response to the current task being taken is correct
+     */
+    @Test
+    public void Test_takeResponse(){
+        System.out.println("Test: taking response from scheduler");
+        Scheduler scheduler = new Scheduler();
+        FireRequest request = new FireRequest("06:20:19", 3, "FIRE_DETECTED", "Low");
+        scheduler.addRequest(request);
+        Response response = new Response(request,"completed");
+        scheduler.addResponse(response);
+
+        Assertions.assertEquals(response, scheduler.takeResponse());
+
+    }
+
+
 }
